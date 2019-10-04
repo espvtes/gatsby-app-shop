bf7a706a13074f709ac73b834f0c092f8fb302c7
Yusniel Espino <espino@Yusniels-MacBook-Pro.local>
2019-10-03 22:33:25 -0400

    Connected to Contenful

--- src/components/Globals/Footer.js
+++ src/components/Globals/Footer.js
@@ -7,8 +7,7 @@ export default class Footer extends React.Component{
                 <div className="contianer">
                     <div className="row">
                         <div className="col-10 mx-auto col-md-6 text-yellow text-center text-capitalize">
-                            {/* <h3>all rights reserved &copy;{new Date().getFullYear().toString()}</h3> */}
-                        <h3>all rights reserved &copy;</h3>
+                            <h3>all rights reserved &copy;{new Date().getFullYear().toString()}</h3>
                         </div>
                     </div>
                 </div>
