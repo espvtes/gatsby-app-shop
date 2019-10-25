be3f016cd5d7dd5f74c7e7916f803459b18abe7b
ESPINO <espvtes@gmail.com>
2019-10-25 01:23:22 +0000

    install and import bootstrap styles

--- gatsby-site/src/components/navbar.js
+++ gatsby-site/src/components/navbar.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 const Navbar = () =>{
     return(
-        <div>Navbar ......</div>
+        <ul className="nav">
+        <li className="nav-item pl-4">
+            <a className="nav-link active" href="#">Active</a>
+        </li>
+        <li className="nav-item">
+            <a className="nav-link" href="#">Link</a>
+        </li>
+        <li className="nav-item">
+            <a className="nav-link" href="#">Link</a>
+        </li>
+        <li className="nav-item">
+            <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+        </ul>
     )
 };
 
