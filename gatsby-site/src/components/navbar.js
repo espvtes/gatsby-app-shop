2d5555e7e40cb444dbf84e9df8a961c801b393e5
ESPINO <espvtes@gmail.com>
2019-10-25 02:32:14 +0000

    content title page and buttons

--- gatsby-site/src/components/navbar.js
+++ gatsby-site/src/components/navbar.js
@@ -6,19 +6,21 @@ const Navbar = () =>{
     return(
         <nav>
         <ul className="nav p-4">
-            <li className="nav-item">
+
                 <Link to="/" className="nav-link text-dark"><img src={Logo} alt="Logo" width="60" height="60" className="d-inline-block align-top"/></Link>
+
+            <li className="nav-item pt-4 pl-2">
+                <Link to="/learnToCode" className="nav-link font-weight-bolder text-muted pt-1">Learn To Code</Link>
             </li>
-            <li className="nav-item">
-                <Link to="/learnToCode" className="nav-link font-weight-bolder text-muted">Learn To Code</Link>
+            <li className="nav-item pt-4 pl-4">
+                <Link to="/aboutUs" className="nav-link font-weight-bolder text-muted pt-1">About Us</Link>
             </li>
-            <li className="nav-item">
-                <Link to="/aboutUs" className="nav-link font-weight-bolder text-muted">About Us</Link>
+            <li className="nav-item pt-4 pl-4">
+                <Link to="/openSourceProject" className="nav-link font-weight-bolder text-muted pt-1">Open Source Project</Link>
             </li>
-            <li className="nav-item">
-                <Link to="/openSourceProject" className="nav-link font-weight-bolder text-muted">Open Source Project</Link>
+            <li className="button pt-4">
+                <button type="button" className="btn btn-outline-primary text-right pt-1">Login</button>
             </li>
-                <button type="button" className="btn btn-outline-primary text-right">Login</button>
         </ul>
         </nav>
     )
--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -3,10 +3,26 @@ import Layout from '../components/layout.js';
 
 
 
+
 const IndexPage = () => {
     return(
         <Layout>
-                <h1>Hello</h1>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                        <h1 className="display-4 font-weight-bold text-center">Welcome to BreatheCo<spam className="text-info">.</spam>de</h1>
+                        <h3 className="font-weigth-bolder text-center text-muted">Learn and get better at coding, using technology.</h3>
+                        </div>
+                    </div>
+                    <div className="row buttons pt-4">
+                        <div className="col-12 col-md-6 text-center">
+                            <button type="button" className="btn btn-outline-success m-2 btn-lg">Contribute</button>
+                        </div>
+                        <div className="col-12 col-md-6 text-center">
+                            <button type="button" className="btn btn-outline-primary  m-2 btn-lg">Start Learning now</button>
+                        </div>
+                    </div>
+                </div>
         </Layout>
 
     )
