59b6363f02fce39d56264ede16a852db52c10edf
ESPINO <espvtes@gmail.com>
2019-10-25 01:28:57 +0000

    links of pages

--- gatsby-site/src/components/navbar.js
+++ gatsby-site/src/components/navbar.js
@@ -3,19 +3,19 @@ import { Link } from 'gatsby';
 
 const Navbar = () =>{
     return(
-        <ul className="nav">
-        <li className="nav-item pl-4">
-            <a className="nav-link active" href="#">Active</a>
-        </li>
-        <li className="nav-item">
-            <a className="nav-link" href="#">Link</a>
-        </li>
-        <li className="nav-item">
-            <a className="nav-link" href="#">Link</a>
-        </li>
-        <li className="nav-item">
-            <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
+        <ul className="nav p-4">
+            <li className="nav-item">
+                <Link to="/" className="nav-link">LOGO OF THE PAGE</Link>
+            </li>
+            <li className="nav-item pl-4">
+                <Link to="/learnToCode" className="nav-link">Learn To Code</Link>
+            </li>
+            <li className="nav-item">
+                <Link to="/aboutUs" className="nav-link">About Us</Link>
+            </li>
+            <li className="nav-item">
+                <Link to="/openSourceProject" className="nav-link">Open Source Project</Link>
+            </li>
         </ul>
     )
 };
