24f48378ac1adb6014110f308dd291ec43abde85
ESPINO <espvtes@gmail.com>
2019-10-25 01:58:22 +0000

    import logo to the navbar

--- gatsby-site/src/components/navbar.js
+++ gatsby-site/src/components/navbar.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Logo from '../images/breathecoLogo.png'
 
 const Navbar = () =>{
     return(
+        <nav>
         <ul className="nav p-4">
             <li className="nav-item">
-                <Link to="/" className="nav-link">LOGO OF THE PAGE</Link>
-            </li>
-            <li className="nav-item pl-4">
-                <Link to="/learnToCode" className="nav-link">Learn To Code</Link>
+                <Link to="/" className="nav-link text-dark"><img src={Logo} alt="Logo" width="60" height="60" className="d-inline-block align-top"/></Link>
             </li>
             <li className="nav-item">
-                <Link to="/aboutUs" className="nav-link">About Us</Link>
+                <Link to="/learnToCode" className="nav-link font-weight-bolder text-muted">Learn To Code</Link>
             </li>
             <li className="nav-item">
-                <Link to="/openSourceProject" className="nav-link">Open Source Project</Link>
+                <Link to="/aboutUs" className="nav-link font-weight-bolder text-muted">About Us</Link>
             </li>
+            <li className="nav-item">
+                <Link to="/openSourceProject" className="nav-link font-weight-bolder text-muted">Open Source Project</Link>
+            </li>
+                <button type="button" className="btn btn-outline-primary text-right">Login</button>
         </ul>
+        </nav>
     )
 };
 
