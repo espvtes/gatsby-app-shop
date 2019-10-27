d721bedfa1eb3946f7cc4939d93f3ea44a071b76
ESPINO <espvtes@gmail.com>
2019-10-27 16:14:58 +0000

    button

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -7,10 +7,10 @@ import Layout from '../components/layout.js';
 const IndexPage = () => {
     return(
         <Layout>
-                <div className="container">
-                    <div className="row">
+                <div className="container pt-4">
+                    <div className="row pt-4">
                         <div className="col-12">
-                        <h1 className="display-4 font-weight-bold text-center">Welcome to BreatheCo<spam className="text-info">.</spam>de</h1>
+                        <h1 className="display-4 font-weight-bold text-center">Welcome to BreatheCo<spam className="text-primary">.</spam>de</h1>
                         <h3 className="font-weigth-bolder text-center text-muted">Learn and get better at coding, using technology.</h3>
                         </div>
                     </div>
