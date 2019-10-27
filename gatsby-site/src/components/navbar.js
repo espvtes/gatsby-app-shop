b1e6cb174cd7cc0b41dea7d4b156d309b30f143f
ESPINO <espvtes@gmail.com>
2019-10-27 15:53:15 +0000

    collapse

--- gatsby-site/src/components/navbar.js
+++ gatsby-site/src/components/navbar.js
@@ -5,14 +5,28 @@ import Logo from '../images/breathecoLogo.png'
 
 const Navbar = () =>{
     return(
-        <nav className="navbar navbar-light bg-light">
-            <Link>Navbar</Link>
-            <a className="navbar-brand">Navbar</a>
-            <a className="navbar-brand">Navbar</a>
-            <a className="navbar-brand">Navbar</a>
-            <form className="form-inline">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="#">Navbar</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li className="nav-item active">
+                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Link</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+                </ul>
+                <form className="form-inline my-2 my-lg-0">
                 <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
+                </form>
+            </div>
         </nav>
     )
 };
