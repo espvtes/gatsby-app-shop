85686ad33eefd4cdae078b6c443771ffa3c1ec58
ESPINO <espvtes@gmail.com>
2019-10-27 15:47:47 +0000

    navbar link

--- gatsby-site/src/components/navbar.js
+++ gatsby-site/src/components/navbar.js
@@ -5,7 +5,15 @@ import Logo from '../images/breathecoLogo.png'
 
 const Navbar = () =>{
     return(
-        <div>navbar</div>
+        <nav className="navbar navbar-light bg-light">
+            <Link>Navbar</Link>
+            <a className="navbar-brand">Navbar</a>
+            <a className="navbar-brand">Navbar</a>
+            <a className="navbar-brand">Navbar</a>
+            <form className="form-inline">
+                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </nav>
     )
 };
 
