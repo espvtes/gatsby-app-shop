d66ccb27f271e2eea06f2eaf77aa6774d68189a7
ESPINO <espvtes@gmail.com>
2019-11-08 03:19:54 +0000

    home page

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -16,46 +16,6 @@ const IndexPage = () => {
                         <h3 className="font-weigth-bolder text-center text-muted"><label className={indexStyles.smallTitle}>Learn and get better at coding, using technology.</label></h3>
                         </div>
                     </div>
-                    <div className="container">
-                        <div className="row pt-2">
-                            <div className="col-12 col-md-5 pt-4 text-center">
-                                <button className="btn btn-outline-success btn-lg">Contribute</button>
-                                <p className={indexStyles.pText}>Thanks to your contributions we have been able
-                                    to publish 2,000 learners, read more about <u>the
-                                    reach and impact of our publications here.</u></p>
-                            </div>
-                            <div className="col-12 col-md-2"></div>
-                            <div className="col-12 col-md-5 pt-4 text-center">
-                                <button className="btn btn-outline-primary btn-lg"><label className={indexStyles}>Start Learning now</label></button>
-                                <p className={indexStyles.pText}>Access interactive tutorials, readings, coding projects and many
-                                other tool powered by technology.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-12">
-                            <h2 className={indexStyles.aboutBreatheCode}>More about BreatheCode</h2>
-                            <div className={indexStyles.text}>We are non-profit that builds technology to enchange the way
-                            you learn and evole your coding skills</div>
-                        </div>
-                        </div>
-                    <div className="row">
-                        <div className="col-12 col-md-6">
-                            <h5>Learn to code</h5>
-                            <Link to="/" className=""><p>1. Project we are building.</p></Link>
-                            <Link to="/" className=""><p>2. People Behind.</p></Link>
-                            <Link to="/" className=""><p>3. Join and/or collaborate.</p></Link>
-                            <Link to="/" className=""><p>4. How do we make money.</p></Link>
-                        </div>
-                        <div className="col-12 col-md-6">
-                            <h5>Organization</h5>
-                            <Link to="/" className=""><p>1. Read: 10min explanations.</p></Link>
-                            <Link to="/" className=""><p>2. Practice: Interactive tutorial.</p></Link>
-                            <Link to="/" className=""><p>3. Code : build project while learning.</p></Link>
-                        </div>
-                    </div>
-                    <h2>Uncoming events</h2>
-                    <p className="text-muted">Share your love for coding and meet other develops.</p>
                 </div>
         </Layout>
 
