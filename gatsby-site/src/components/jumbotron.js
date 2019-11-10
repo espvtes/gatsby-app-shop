4d5e3ed73a8f384073f988364b4dcdc68279b178
ESPINO <espvtes@gmail.com>
2019-11-10 20:07:32 +0000

    styles jumbotron

--- gatsby-site/src/components/jumbotron.js
+++ gatsby-site/src/components/jumbotron.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import JumbotronStyles from '../styles/components.module.css';
+import TitleStyles from '../styles/components.module.css';
+import WelcomeStyles from '../styles/components.module.css';
 
 
 const Jumbotron = () => {
     return(
         <div className={JumbotronStyles.jumb}>
             <div className="container">
-                <h1 className="display-3 text-center">Welcome to BreatheCo<spam className="text-primary">.</spam>de</h1>
-                <p className="lead text-center">Learn and get better at coding, using technology.</p>
+                <h1 className={WelcomeStyles.welcome}>Welcome to BreatheCo<spam className="text-primary">.</spam>de</h1>
+                <p className={TitleStyles.title}>Learn and get better at coding, using technology.</p>
             </div>
         </div>
     )
