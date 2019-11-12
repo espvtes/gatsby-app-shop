a095a808eb92e683bc09df80188a23d938229176
ESPINO <espvtes@gmail.com>
2019-11-12 03:22:53 +0000

    home page

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -12,13 +12,36 @@ const IndexPage = () => {
     return(
         <Layout>
                 <div className="container pt-4">
-                <Jumbotron/>
-                <Buttons/>
+                    <Jumbotron/>
+                    <Buttons/>
+                        <div className="row">
+                            <div className="col-12 col-md-6 text-center">
+                            Thanks to your constribution we've been able to
+                            publish 2,000 learners, read more about the reach 
+                            and impact of our publications here</div>
+                            <div className="col-12 col-md-6 text-center">Access interactive tutorials, readings, coding project
+                            and many other tools powered by tecnhology</div>
+                        </div>
+                    <h2>More about BreatheCode</h2>
+                    <p>We are a non-profit that builds tecnhology to enchange the way you learn and evole your coding skills</p>
                     <div className="row">
-                        <div className="col-12 col-md-6 text-center">Thanks to your constribution we've been able to publish</div>
-                        <div className="col-12 col-md-6 text-center">Thanks to your constribution we've been able to publish</div>
+                        <div className="col-12 col-md-6">
+                            <h5>Learn to code</h5>
+                            <p>1.Project we are building</p>
+                            <p>2.People Behind.</p>
+                            <p>3.Join and/or collaborate.</p>
+                            <p>4.How do we make money?</p>
+                        
+                        </div>
+                        <div className="col-12 col-md-6">
+                            <h5>Organization</h5>
+                            <p>1.Read: 10 min explanations.</p>
+                            <p>2.Practice: Interactive tutorials.</p>
+                            <p>3.Code: Build projects while learning.</p>
+                        </div>
                     </div>
-
+                    <h2>Upcoming  events </h2>
+                    <p>Share your love for coding and meet other developers.</p>
                 </div>
         </Layout>
 
